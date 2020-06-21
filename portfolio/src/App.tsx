49a8f789e36ccc5214b0ff630e79c70d6d65c327
suizyu <suizyu_m@yahoo.co.jp>
2020-06-21 21:35:43 +0900

    画面リサイズ対応

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -9,19 +9,42 @@ class App extends React.Component {
   constructor(props: any) {
     super(props);
     this.animate = this.animate.bind(this);
+    this.onResize();
   }
 
   onCanvasLoaded = (canvas: HTMLCanvasElement) => {
     if (!canvas) {
       return;
-    }
+    } 
     this.renderer = new WebGLRenderer({ canvas: canvas, antialias: true });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.scene = new Scene();
     this.scene.background = new Color( 0xbfe3dd );
-    this.camera = new PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 1, 2000);
-    this.animate()
+    this.camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    this.animate();
   };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize, false);
+  }
+
+  onResize = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    if (this.renderer) {
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+      this.renderer.setSize(width, height);
+    }
+
+    if (this.camera){
+      this.camera.aspect = width/height;
+      this.camera.updateProjectionMatrix();
+    }
+  }
 
   animate() {
     window.requestAnimationFrame(this.animate);
