97792fcce871e7c02d71f433c7b21ee360e4796d
suizyu <suizyu_m@yahoo.co.jp>
2020-09-01 23:46:38 +0900

    メニューを画面左、縦表示に変更。topが画面外になった後、順次表示するアニメーションを追加

--- portfolio/src/component/navigation/navigation.tsx
+++ portfolio/src/component/navigation/navigation.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import style from './navigation.module.css';
+import { useTrail, animated } from 'react-spring';
 
 interface ScrollLinkProps {
     to: string;
+    offset?: number | null;
+}
+
+interface MenuProps {
+}
+
+interface isOnDisplay {
+    isView: boolean;
 }
 
 class ScrollLink extends React.Component<ScrollLinkProps> {
     render(){
-        const offset = window.innerHeight / 7;
+        const offset = this.props.offset == null ? -10 : window.innerHeight / this.props.offset;
         return(
             <Link activeClass="active" 
                 to={this.props.to} 
@@ -20,29 +29,52 @@ class ScrollLink extends React.Component<ScrollLinkProps> {
     }
 }
 
-class MenuBar extends React.Component {
+const AnimateMenuItem = (props: isOnDisplay) => {
+    const menuItems = [
+        <ScrollLink to="/">top</ScrollLink>,
+        <ScrollLink to="/about">about</ScrollLink>,
+        <ScrollLink to="/images">images</ScrollLink>,
+        <ScrollLink to="/skills">skills</ScrollLink>,
+        <ScrollLink to="/contact">contact</ScrollLink>
+    ];
+    const config = { mass: 5, tension: 1500, friction: 200 };
+    const trail = useTrail(menuItems.length, {
+        config,
+        opacity: props.isView ? 1 : 0,
+        x: props.isView ? 0 : -20,
+        height: 50,
+        from: { opacity: 0, x: -20, height: 0 },
+    })
+
+    return(
+        <>
+            {trail.map(({ x, height, ...rest }, index) => (
+                <animated.div
+                    key={index}
+                    className={props.isView ? style.menuBtn : style.hidenMenu}
+                    style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`), height }}>
+                    {menuItems[index]}
+                </animated.div>
+            ))}
+        </>
+    );
+}
+
+class MenuBar extends React.Component<isOnDisplay> {
     render(){
         return(
             <div className={style.menu}>
-                <ScrollLink to="/">top</ScrollLink>
-                <ScrollLink to="/about">about</ScrollLink>
-                <ScrollLink to="/images">images</ScrollLink>
-                <ScrollLink to="/skills">skills</ScrollLink>
-                <ScrollLink to="/contact">contact</ScrollLink>
+                <AnimateMenuItem isView={this.props.isView} />
             </div>
         );
     }
 }
 
 class Footer extends React.Component {
-    //仮
     render(){
         return(
             <div className={style.footer}>
-                <ScrollLink to="/">top</ScrollLink>
-                <ScrollLink to="/about">about</ScrollLink>
-                <ScrollLink to="/images">images</ScrollLink>
-                <ScrollLink to="/contact">contact</ScrollLink>
+                <span>@suizyu</span>
             </div>
         );
     }
