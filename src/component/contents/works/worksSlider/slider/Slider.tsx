dc2d196bd33eb84db78b7e09ebf485cdf2ee9a71
suizyu <suizyu_m@yahoo.co.jp>
2020-09-07 22:55:03 +0900

    #12 モーダルの閉じるボタンにアニメーションを追加。

--- src/component/contents/works/worksSlider/slider/Slider.tsx
+++ src/component/contents/works/worksSlider/slider/Slider.tsx
@@ -1,14 +1,15 @@
 import React, { useState, FormEvent } from 'react';
-import Slick from 'react-slick';
 import style from './slider.module.css';
-import { Overlay } from 'react-portal-overlay';
-import { Scrollbars } from 'react-custom-scrollbars';
+import Slick from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { Overlay } from 'react-portal-overlay';
+import { Scrollbars } from 'react-custom-scrollbars';
 import worksData from '../../worksData.json';
 import SignUpFlow from '../../details/signUpFlow/SignUpFlow';
 import PortfolioWork from '../../details/portfolio/Portfolio';
 import TrimmingApp from '../../details/triming/TrimingApp';
+import { useSpring, animated } from 'react-spring';
 
 const detailContents = [ 
   { key: "signUp" , detail: <SignUpFlow /> },
@@ -23,6 +24,51 @@ interface thmbnailProps {
   modify: string
 };
 
+const CloseBnt = () => {
+  const [isHover, setIsHover] = useState(false);
+  const  { ...props} = useSpring({
+    to: { 
+      transform: isHover ? "scale(1)" : "scale(0.3)",
+      opacity: isHover ? 1 : 0
+    },
+    from: { 
+      transform: "scale(0.1)", 
+      opacity: 0
+    },
+    config: { tension: 350 },
+    });
+
+    const SpringRound = () => {
+      return(
+        <animated.div
+          className={style.springRound}
+          onMouseMove={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false) }
+          style={{ ...props}} />
+      );
+    }
+
+    const {...colorSetting } = useSpring({
+      to: { color: isHover ? "rgba(255,255,255)" : "rgba(148,187,233)" },
+      from: { color: "rgba(148,187,233)" }
+    })
+    const CloseIcon = () => {
+      return(
+        <animated.div className={style.closeIcon} 
+          style={{ ...colorSetting}}>
+            ×
+        </animated.div>
+      );
+    }
+
+    return (
+      <div className={style.btnBox}>
+        <SpringRound />
+        <CloseIcon />
+      </div>
+    );
+}
+
 const Thumbnail = (props: thmbnailProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const modalBackgroundStyle = {
@@ -35,7 +81,6 @@ const Thumbnail = (props: thmbnailProps) => {
 
   function closeEvent(event: FormEvent , closing: boolean) {
     event.stopPropagation();
-    console.log(closing);
     if (closing) {
       setIsOpen(false);
     }
@@ -55,7 +100,9 @@ const Thumbnail = (props: thmbnailProps) => {
           <Overlay style={modalBackgroundStyle} open={isOpen} onClick={event => closeEvent(event, true)}>
               <div className={style.modal} onClick={event => closeEvent(event, false)}>
                 <Scrollbars>
-                    <div className={style.closebtn} onClick={event => closeEvent(event, true)}><span>×</span></div>
+                    <div className={style.closebtn} onClick={event => closeEvent(event, true)}>
+                      <CloseBnt />
+                    </div>
                     <div className={style.worksDetail}>
                       <p>{ workYearMonth }</p>
                         {detail}
