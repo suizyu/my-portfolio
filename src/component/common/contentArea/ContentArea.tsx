74d2e3c3324aa3885c8964c50285e43a57ba16fe
suizyu <suizyu_m@yahoo.co.jp>
2020-09-08 21:54:23 +0900

    #4 コンテンツ表示コンテナのモバイル対応。

--- src/component/common/contentArea/ContentArea.tsx
+++ src/component/common/contentArea/ContentArea.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import style from './contetnArea.module.css';
 
-class ContentArea extends React.Component {
-    render (){
+interface MediaState {
+    isPC: boolean;
+}
+
+class ContentArea extends React.Component<{}, MediaState> {
+    constructor(props: any) {
+        super(props);
+        this.state = { isPC: true };
+    }
+
+    onResize = () => {
+        if(window.matchMedia("(max-width: 480px)").matches) {
+            this.setState({ isPC: false });
+        } else {
+            this.setState({ isPC: true });
+        }
+    }
+
+    //リサイズイベント
+    componentDidMount = () => {
+        window.addEventListener('resize', this.onResize, false);
+    }
+    componentWillUnmount = () => {
+        window.removeEventListener('resize', this.onResize, false);
+    }
+
+    render() {
         return (
             <div className={style.fullSizeContainer}>
-                <div className={style.contentsContainer}>
+                <div className={ this.state.isPC ? style.contentsContainer : style.mobileContainer }>
                     {this.props.children}
                 </div>
             </div>
