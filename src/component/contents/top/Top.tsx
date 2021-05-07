b52f8e0aaa6efd98998b71ba7b78964f5b7b9028
suizyu <suizyu_m@yahoo.co.jp>
2021-05-07 13:52:22 +0900

    トップ画面の高さを調整。スクロールダウンを追加。

--- src/component/contents/top/Top.tsx
+++ src/component/contents/top/Top.tsx
@@ -9,8 +9,20 @@ const Top = () => {
                     Tomomi Nemoto's <br/>
                     Portfolio
                 </div>
+                <ScrollGuide />
             </div>
         </div>
+    );
+}
+
+const ScrollGuide = () => {
+    return(
+        <svg width="250" height="100" id="guide">
+            <path d="M 10 10 H 150 V 150 H 10 Z" 
+            fill-rule="evenodd" fill-opacity="0" stroke-width="5" stroke="white" transform="rotate(45), translate(0,-170)" />
+            <path d=" M 30 30 V 130 H 130 V 30 Z" 
+                fill-rule="evenodd" fill-opacity="0" stroke-width="15" stroke="white" transform="rotate(45), translate(0,-170)" />
+        </svg>
     );
 }
 
